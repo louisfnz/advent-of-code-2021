d3760592fab844db4efb11f15fe8f9f99fb101d4
Louis <25600218+louisfnz@users.noreply.github.com>
2021-12-01 18:19:59 +1300

    Day 1

---
+++ day-1/part-1.ts
@@ -0,0 +1,21 @@
+import {readFile} from 'fs/promises';
+
+const run = async () => {
+    const input = await readFile(__dirname + '/input.txt', 'utf-8');
+    const entries = input.trim().split('\n').map((entry: string) => Number(entry.trim()));
+
+    let prev: number | undefined = undefined;
+    let count = 0;
+
+    for (const entry of entries) {
+        if (prev && prev < entry) {
+            count++;
+        }
+        prev = entry;
+    }
+
+    return count;
+}
+
+run().then(result => console.log(`Result: ${result}`));
+
---
+++ day-1/part-2.ts
@@ -0,0 +1,22 @@
+import {readFile} from 'fs/promises';
+
+const run = async () => {
+    const input = await readFile(__dirname + '/input.txt', 'utf-8');
+    const entries = input.trim().split('\n').map((entry: string) => Number(entry.trim()));
+
+    let prev: number | undefined;
+    let count = 0;
+
+    for (let i = 2; i < entries.length; i++) {
+        const sum = entries[i - 2] + entries[i - 1] + entries[i];
+        if (prev && prev < sum) {
+            count++;
+        }
+        prev = sum;
+    }
+
+    return count;
+}
+
+run().then(result => console.log(`Result: ${result}`));
+
